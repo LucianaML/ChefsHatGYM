454bdca36c9a14aba6116efa7225021c58db358f
Gabriel <gabriellima.glgs@gmail.com>
2024-12-04 15:22:50 -0300

    adding local rooms

--- examples/local_room_train.py
+++ examples/local_room_train.py
@@ -2,18 +2,19 @@ from ChefsHatGym.gameRooms.chefs_hat_room_local import ChefsHatRoomLocal
 from ChefsHatGym.env import ChefsHatEnv
 from ChefsHatGym.agents.agent_random import AgentRandon
 from ChefsHatGym.agents.spectator_logger import SpectatorLogger
-from ChefsHatGym.agents.dqn_agent import FirstAgent
+from ChefsHatGym.agents.dqn_agent import DQNAgent
+from ChefsHatGym.agents.agent_with_classification import AgentClassification
 from ChefsHatGym.agents.classic.dql import AgentDQL
 from ChefsHatGym.agents.classic.ppo import AgentPPO
 
 # Room parameters
-room_name = "test_vsRandom"
+room_name = "train_vsRandom"
 timeout_player_response = 5
 
 
 # Game parameters
 game_type = ChefsHatEnv.GAMETYPE["MATCHES"]
-stop_criteria = 100 #300  matches usados para treinar pela ultima vez!! treino 18
+stop_criteria = 10 #300  matches usados para treinar pela ultima vez!! treino 18
 maxRounds = -1
 
 # Logging information
@@ -44,16 +45,22 @@ logDirectory = room.get_log_directory()
 agentVerbose = True
 
 # Create players
-p1 = FirstAgent(name="01", log_directory=logDirectory, verbose_log=agentVerbose, continue_training=False, use_phase=False)
+p1 = DQNAgent(name="01", log_directory=logDirectory, verbose_log=agentVerbose, continue_training=True, use_phase=False)
+p2 = AgentClassification(name="02", log_directory=logDirectory, verbose_log=agentVerbose, continue_training=True, use_phase=False)
+p3 = DQNAgent(name="03", log_directory=logDirectory, verbose_log=agentVerbose, continue_training=True, use_phase=False)
+p4 = DQNAgent(name="04", log_directory=logDirectory, verbose_log=agentVerbose, continue_training=True, use_phase=False)
+#p2 = AgentClassification(name="02", log_directory=logDirectory, verbose_log=agentVerbose, continue_training=True, use_phase=False)
+#p3 = AgentClassification(name="03", log_directory=logDirectory, verbose_log=agentVerbose, continue_training=True, use_phase=False)
+#p4 = AgentClassification(name="04", log_directory=logDirectory, verbose_log=agentVerbose, continue_training=True, use_phase=False)
 #p2 = AgentDQL(name="02", log_directory=logDirectory, verbose_log=agentVerbose, agentType="vsEveryone", continueTraining=False)
 #p3 = AgentPPO(name="03", log_directory=logDirectory, verbose_log=agentVerbose, agentType="vsEveryone", continueTraining=False, saveFolder="")
 #p2 = FirstAgent(name="02", log_directory=logDirectory, verbose_log=agentVerbose, continue_training=False)
 #p3 = FirstAgent(name="03", log_directory=logDirectory, verbose_log=agentVerbose, continue_training=False)
 #p4 = FirstAgent(name="04", log_directory=logDirectory, verbose_log=agentVerbose, continue_training=False)
 #p1 = AgentRandon(name="01", log_directory=logDirectory, verbose_log=agentVerbose)
-p2 = AgentRandon(name="02", log_directory=logDirectory, verbose_log=agentVerbose)
-p3 = AgentRandon(name="03", log_directory=logDirectory, verbose_log=agentVerbose)
-p4 = AgentRandon(name="04", log_directory=logDirectory, verbose_log=agentVerbose)
+# p2 = AgentRandon(name="02", log_directory=logDirectory, verbose_log=agentVerbose)
+# p3 = AgentRandon(name="03", log_directory=logDirectory, verbose_log=agentVerbose)
+# p4 = AgentRandon(name="04", log_directory=logDirectory, verbose_log=agentVerbose)
 
 # Adding players to the room
 for p in [p1, p2, p3, p4]:
